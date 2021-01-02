82f0412fdf4e3ec09e7b11d3fe5865175e6800fd
mu001999 <mu001999@outlook.com>
2021-01-02 16:16:27 +0800

    update test

--- test.cpp
+++ test.cpp
@@ -1,6 +1,14 @@
 #include "nameof.hpp"
 #include <iostream>
 
+namespace A
+{
+namespace B
+{
+struct C {};
+}
+}
+
 int main()
 {
     std::cout << nameof::nameof<int> << std::endl;
@@ -8,4 +16,8 @@ int main()
     std::cout << nameof::nameof<int&> << std::endl;
     std::cout << nameof::nameof<int const &> << std::endl;
     std::cout << nameof::nameof<int (&)[12]> << std::endl;
+    std::cout << nameof::nameof<A::B::C> << std::endl;
+
+    using D = A::B::C;
+    std::cout << nameof::nameof<D> << std::endl;
 }
